f36cd102cf7cc0447c866128719a33b709100e03
Mateus Silva <mateusilva.dev@gmail.com>
2022-11-16 17:18:24 -0300

    add orders mock

---
+++ aula-2/mocks/orders.ts
@@ -0,0 +1,27 @@
+const orders: Order[] = [
+  {
+    _id: '6372e48cbcd195b0d3d0f7f3',
+    table: '123',
+    status: 'WAITING',
+    products: [
+      {
+        product: {
+          name: 'Pizza quatro queijos',
+          imagePath: '1668472896991-quatro-queijos.png',
+          price: 40,
+        },
+        quantity: 3,
+        _id: '6372e48cbcd195b0d3d0f7f4'
+      },
+      {
+        product: {
+          name: 'Coca cola',
+          imagePath: '1668473462705-coca-cola.png',
+          price: 7,
+        },
+        quantity: 2,
+        _id: '6372e48cbcd195b0d3d0f7f5'
+      }
+    ],
+  }
+];
